0f9dd8db3ebbd3abbe53fa442d48a2061db9b58d
Alexey S. Brovikov <alexey.brovikov@gmail.com>
2016-05-22 20:47:29 +0700

    add color thema

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome', to: 'static#welcome'
 
+  root to: 'static#welcome'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
